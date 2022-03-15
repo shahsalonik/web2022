9ec794eef1326bb337f55f03ef270a98b6aacfa9
Saloni Shah <shahsalonik@gmail.com>
2022-03-15 14:14:04 -0400

    s

--- public/routes/votes.js
+++ public/routes/votes.js
@@ -5,8 +5,42 @@ router.get('/', function(req, res) {
     res.render('dino_votes');
 });
 
-router.get('/view_dino_votes', function (req, res) {
-    res.render('view_votes')
+router.get('/view_dino_votes', function(req, res) {
+    var https = require('https');
+    var url = 'https://user.tjhsst.edu/2023sshah/dinos'
+    
+    var options = { 
+    	headers : {
+    		'User-Agent': 'request'
+    	}
+    }
+
+    https.get(url, options, function(response) {
+    	var rawData = '';
+    	response.on('data', function(chunk) {
+    		rawData += chunk;
+    	});
+    	response.on('end', function() {
+            console.log(rawData); // THIS IS WHERE YOU HAVE ACCESS TO RAW DATA
+            var obj = JSON.parse(rawData);
+            console.log(obj);
+            
+            var render_info = {
+                'id' : obj[0].d_id,
+                'name' : obj.d_name,
+                'votes' : obj.votes,
+            }
+            console.log(render_info);
+            console.log(obj);
+            res.render('view_votes', render_info)
+            
+        });
+    }).on('error', function(e) {
+    	console.error(e);
+    })
+    
+    
+    
 });
 
 module.exports = router;
