a21a41174f88fee000ac8bc1309bf4d16b7f20dd
Saloni Shah <shahsalonik@gmail.com>
2022-03-17 13:46:49 -0400

    finished SQL info

--- public/routes/votes.js
+++ public/routes/votes.js
@@ -6,38 +6,30 @@ router.get('/', function(req, res) {
 });
 
 router.get('/view_dino_votes', function(req, res) {
-    var https = require('https');
-    var url = 'https://user.tjhsst.edu/2023sshah/dinos'
+    var sqlQuery;
     
-    var options = { 
-    	headers : {
-    		'User-Agent': 'request'
-    	}
+    if(req.query.votes == 0) {
+        sqlQuery = "UPDATE dinos SET votes = votes + 1 WHERE d_name = 'Rex';";
     }
-
-    https.get(url, options, function(response) {
-    	var rawData = '';
-    	response.on('data', function(chunk) {
-    		rawData += chunk;
-    	});
-    	response.on('end', function() {
-            console.log(rawData); // THIS IS WHERE YOU HAVE ACCESS TO RAW DATA
-            var obj = JSON.parse(rawData);
-            console.log(obj);
-            
-            var render_info = {
-                dino : obj,
-            }
-            console.log(render_info);
-            console.log(obj);
-            res.render('view_votes', render_info)
-            
+    else if(req.query.votes == 1) {
+        sqlQuery = "UPDATE dinos SET votes = votes + 1 WHERE d_name = 'John';";
+    }
+    else if(req.query.votes == 2) {
+        sqlQuery = "UPDATE dinos SET votes = votes + 1 WHERE d_name = 'Asteroid';";
+    }
+    else {
+        sqlQuery = "";
+    }
+    res.app.locals.pool.query(sqlQuery, function(error,results,fields){
+        if (error) throw error;
+        var sql = "SELECT d_id, d_name, votes FROM dinos;";
+        res.app.locals.pool.query(sqlQuery, function(error,results,fields){
+            if (error) throw error;
+            res.render('view_votes', results);
         });
-    }).on('error', function(e) {
-    	console.error(e);
-    })
-    
-    
+    });
+
+     
     
 });
 
