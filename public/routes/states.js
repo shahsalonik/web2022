fdb2a83db189169e08f9738cc8fcbe4fda7f9bf9
Saloni Shah <shahsalonik@gmail.com>
2022-06-04 18:08:19 -0400

    states game: generating states the user must pick

--- public/routes/states.js
+++ public/routes/states.js
@@ -1,8 +1,19 @@
 const express = require('express');
-const router = express.Router()
+const router = express.Router();
 
 router.get('/', function(req, res) {
-    res.render('states')
-})
+    res.render('states');
+});
+
+router.get('/state', function(req, res) {
+    var input = req.query.getState;
+    console.log("State: " + input);
+    
+    var params = {
+        'state' : input,
+    };
+    console.log(params);
+    res.json(params);
+});
 
 module.exports = router;
